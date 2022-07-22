c98f6c8a7128fc93fb6a3a313c206da95072b598
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-07-22 13:56:41 -0400

    Database seeding

--- backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.j2emme.dscatalog.entities.Category;
 import com.j2emme.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Category> findall() {
 		return repository.findAll();
 	}
