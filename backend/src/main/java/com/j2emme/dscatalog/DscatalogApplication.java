c3c518c3d425db92acc1aefc4c5dcce36c310645
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-07-22 00:18:37 -0400

    Backend project created

---
+++ backend/src/main/java/com/j2emme/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.j2emme.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
