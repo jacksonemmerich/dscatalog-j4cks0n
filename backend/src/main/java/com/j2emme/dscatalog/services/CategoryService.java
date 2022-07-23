8af61232cdfe96b0224cbff4f6f8e439e9beecda
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-07-23 10:58:49 -0400

    DTO

--- backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.j2emme.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.j2emme.dscatalog.dto.CategoryDTO;
 import com.j2emme.dscatalog.entities.Category;
 import com.j2emme.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,17 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findall() {
-		return repository.findAll();
+	public List<CategoryDTO> findall() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+//		outro jeito de fazer mais verboso
+//		List<CategoryDTO> listDto = new ArrayList<>();
+//		for(Category cat : list) {
+//			listDto.add(new CategoryDTO(cat));
+//		}
+//		return listDto;
+
 	}
 }
