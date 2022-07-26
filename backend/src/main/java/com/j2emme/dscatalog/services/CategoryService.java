3dc1476801b34e4c6bdf2d7b0c775fd425a89d58
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-07-26 14:10:05 -0400

    busca por id em categorias

--- backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/j2emme/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.j2emme.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +24,12 @@ public class CategoryService {
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
-//		outro jeito de fazer mais verboso
-//		List<CategoryDTO> listDto = new ArrayList<>();
-//		for(Category cat : list) {
-//			listDto.add(new CategoryDTO(cat));
-//		}
-//		return listDto;
+	}
 
+	@Transactional(readOnly = true)
+	public CategoryDTO findaById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 }
