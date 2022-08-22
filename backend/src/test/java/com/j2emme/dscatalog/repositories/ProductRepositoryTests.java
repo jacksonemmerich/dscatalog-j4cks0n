09fba8e9e8603036d3cc5833f6cc28ecff501481
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-08-22 00:16:17 -0400

    02-21 Exercicio com repository

--- backend/src/test/java/com/j2emme/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/j2emme/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,12 +3,14 @@ package com.j2emme.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.j2emme.dscatalog.entities.Product;
+import com.j2emme.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -16,10 +18,51 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private long exintingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		exintingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void findByIdReturnOptionalNotNullWhenIdExists() {
+		repository.findById(exintingId);
+		
+		Optional<Product> result = repository.findById(exintingId);
+		
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void findByIdReturnOptionalEmptyWhenIdNotExists( ) {
+		repository.findById(nonExistingId);
+		
+		Optional<Product> result = repository.findById(nonExistingId);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	
+	
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+		
+	}
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		
-		long exintingId = 1L;
 		
 		repository.deleteById(exintingId);
 		
@@ -30,8 +73,6 @@ public class ProductRepositoryTests {
 	
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-		
-		long nonExistingId = 1000L;
 		
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
---
+++ backend/src/test/java/com/j2emme/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.j2emme.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.j2emme.dscatalog.dto.ProductDTO;
+import com.j2emme.dscatalog.entities.Category;
+import com.j2emme.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, Instant.parse("2020-10-20T03:00:00Z"), "https://img.com/img.png");
+		product.getCategories().add(new Category(1L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+}
