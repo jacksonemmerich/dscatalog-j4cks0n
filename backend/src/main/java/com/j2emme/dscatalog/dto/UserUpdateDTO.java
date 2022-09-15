4c214c875228992e09a1eab86b04d05d94305fb1
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-09-15 10:41:09 -0400

    UserUpdateValidator

---
+++ backend/src/main/java/com/j2emme/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.j2emme.dscatalog.dto;
+
+import com.j2emme.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
--- backend/src/main/java/com/j2emme/dscatalog/services/UserService.java
+++ backend/src/main/java/com/j2emme/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.j2emme.dscatalog.dto.RoleDTO;
 import com.j2emme.dscatalog.dto.UserDTO;
 import com.j2emme.dscatalog.dto.UserInsertDTO;
+import com.j2emme.dscatalog.dto.UserUpdateDTO;
 import com.j2emme.dscatalog.entities.Role;
 import com.j2emme.dscatalog.entities.User;
 import com.j2emme.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getReferenceById(id);
 			copyDtoToEntity(dto, entity);
