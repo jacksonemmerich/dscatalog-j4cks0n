2eb01a75175e093dde7b6e95b4b5340163ed657d
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-09-20 13:54:27 -0400

    Segunda parte da Impl Jwt e oauth2, com melhoria no retorno do token id e firstName

--- backend/src/main/java/com/j2emme/dscatalog/config/WeBSecurityConfig.java
+++ backend/src/main/java/com/j2emme/dscatalog/config/WeBSecurityConfig.java
@@ -17,16 +17,15 @@ public class WeBSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/actuator/**");
@@ -37,10 +36,5 @@ public class WeBSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
-	
 
-	
-	
-	
 }
