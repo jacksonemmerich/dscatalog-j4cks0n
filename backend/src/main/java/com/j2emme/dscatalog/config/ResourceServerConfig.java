2d9c38213cbb0208da2fc002185e470aa879c048
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-09-20 23:03:47 -0400

    H2 database corrigido para aparecer

--- backend/src/main/java/com/j2emme/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/j2emme/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.j2emme.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,9 +17,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	//config de acesso public e test
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
@@ -26,10 +33,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
 	}
-
 	
 	@Override
     public void configure(HttpSecurity http) throws Exception {
+		
+		//conf para liberar H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
